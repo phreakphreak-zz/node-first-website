32c8c5e9c190f3bbfd7b41b73d46bac013fdb6bd
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2020-10-05 16:27:06 -0400

    ajuste de motor de plantilla de ejs a html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 //*settings
-app.set('port',3000);
-app.set('views',path.join(__dirname+'/views'))
-app.set('view engine','ejs');
+app.set("port", 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine('html',require('ejs').renderFile);
+app.set("view engine", "ejs");
 
 //*routes
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
+
+//*static files
+app.use(express.static(path.join(__dirname,'public')))
 
 //*listening server
-app.listen(app.get('port'),()=>{
-    console.log('server on port',app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("server on port", app.get("port"));
 });
